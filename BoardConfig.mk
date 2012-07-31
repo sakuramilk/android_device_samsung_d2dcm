a8457ece089c53a90c2c598a7f30925315d0e0c5
sakuramilk <c.sakuramilk@gmail.com>
2012-08-01 00:14:57 +0900

    import from ics branch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,18 +24,20 @@
 -include device/samsung/d2-common/BoardConfigCommon.mk
 
 # inherit from the proprietary version
--include vendor/samsung/d2tmo/BoardConfigVendor.mk
+-include vendor/samsung/d2dcm/BoardConfigVendor.mk
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := d2tmo
+TARGET_OTA_ASSERT_DEVICE := d2dcm
 
 # Kernel
-TARGET_KERNEL_SOURCE        := kernel/samsung/d2
-TARGET_KERNEL_CONFIG        := cyanogen_d2_defconfig
-BOARD_KERNEL_CMDLINE        := androidboot.hardware=qcom user_debug=31
-BOARD_KERNEL_BASE           := 0x80200000
-BOARD_FORCE_RAMDISK_ADDRESS := 0x81500000
-BOARD_KERNEL_PAGESIZE       := 2048
+#TARGET_KERNEL_SOURCE        := kernel/samsung/d2
+#TARGET_KERNEL_CONFIG        := cyanogen_d2_defconfig
+#BOARD_KERNEL_CMDLINE        := androidboot.hardware=qcom user_debug=31
+#BOARD_KERNEL_BASE           := 0x80200000
+#BOARD_FORCE_RAMDISK_ADDRESS := 0x81500000
+#BOARD_KERNEL_PAGESIZE       := 2048
+
+TARGET_PREBUILT_KERNEL := device/samsung/d2dcm/kernel
 
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00A00000
